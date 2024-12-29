702ea955315e230d2fd40b9ff629c193d23db301
salarpro <salarkhalid97@gmail.com>
2024-12-29 15:55:39 +0300

    error handling

--- src/ImageAnalyzer.py
+++ src/ImageAnalyzer.py
@@ -48,101 +48,116 @@ class ImageAnalyzer:
             return 80
 
     def compare_faces(self, image_path):
-        
-        # if image file start with done_ then return
-        if image_path.split('\\')[-1].startswith('done_'):
+        try:
+            # if image file start with done_ then return
+            if image_path.split('\\')[-1].startswith('done_'):
+                return "Unknown", -1
+            
+            image = cv2.imread(image_path)
+            if image is None:
+                print(f"Error: Could not read image {image_path}")
+                return "Unknown", 0
+
+            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            face_locations = face_recognition.face_locations(rgb_image)
+            face_encodings = face_recognition.face_encodings(rgb_image, face_locations)
+
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                best_match_index = None
+                best_match_score = float('inf')
+
+                for index, (match, distance) in enumerate(zip(matches, face_distances)):
+                    if match and distance < best_match_score:
+                        best_match_score = distance
+                        best_match_index = index
+
+                if best_match_index is not None and (1 - best_match_score) * 100 >= self.accepted_percentage:
+                    name = self.known_face_names[best_match_index]
+                    return name, (1 - best_match_score) * 100
+        except Exception as e:
+            print(f"Error: {e}")
             return "Unknown", -1
-        
-        image = cv2.imread(image_path)
-        if image is None:
-            print(f"Error: Could not read image {image_path}")
-            return "Unknown", 0
-
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        face_locations = face_recognition.face_locations(rgb_image)
-        face_encodings = face_recognition.face_encodings(rgb_image, face_locations)
-
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-            best_match_index = None
-            best_match_score = float('inf')
-
-            for index, (match, distance) in enumerate(zip(matches, face_distances)):
-                if match and distance < best_match_score:
-                    best_match_score = distance
-                    best_match_index = index
-
-            if best_match_index is not None and (1 - best_match_score) * 100 >= self.accepted_percentage:
-                name = self.known_face_names[best_match_index]
-                return name, (1 - best_match_score) * 100
-
         return "Unknown", 0
 
     def record_attendee(self, name, percentage, image_path):
+        try:
+            # filePahtName
+            # (i|o)I(time).jpg
+            # ex: i_1735393560.4280732.jpg
+            # image path start with i or o, i is for in and o is for out
+            print(image_path) # ./user_images_queue\i_1735402052.2187057.jpg
+            fileName = image_path.split('\\')[-1]
+            type = fileName[0]
+            timeStr = fileName.split('_')[1].split('.')[0]
 
-        # filePahtName
-        # (i|o)I(time).jpg
-        # ex: i_1735393560.4280732.jpg
-        # image path start with i or o, i is for in and o is for out
-        print(image_path) # ./user_images_queue\i_1735402052.2187057.jpg
-        fileName = image_path.split('\\')[-1]
-        type = fileName[0]
-        timeStr = fileName.split('_')[1].split('.')[0]
+            print(type)
+            if type == 'i':
+                type = 'in'
+            elif type == 'o':
+                type = 'out'
+            else:
+                os.remove(image_path)
+                return
+            
+            # rename image file add done to the beginning of the file name
+            os.rename(image_path, f"./user_images_queue/done_{fileName}")
+            image_path = f"./user_images_queue/done_{fileName}"
+            
+            # timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(timeStr)))
+            # date = time.strftime("%Y-%m-%d", time.localtime())
+            # data = {
+            #     "name": name,
+            #     "timestamp": timestamp,
+            #     "percentage": percentage,
+            #     "image": image_path,
+            #     "sent_to_server": False,
+            #     "server_response": None,
+            #     "type": type
+            # }
+            # name start with number_string
+            userId = name.split('_')[0]
+            userName = name.split('_')[1]
 
-        print(type)
-        if type == 'i':
-            type = 'in'
-        elif type == 'o':
-            type = 'out'
-        else:
-            os.remove(image_path)
-            return
-        
-        # rename image file add done to the beginning of the file name
-        os.rename(image_path, f"./user_images_queue/done_{fileName}")
-        image_path = f"./user_images_queue/done_{fileName}"
-        
-        # timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(timeStr)))
-        # date = time.strftime("%Y-%m-%d", time.localtime())
-        # data = {
-        #     "name": name,
-        #     "timestamp": timestamp,
-        #     "percentage": percentage,
-        #     "image": image_path,
-        #     "sent_to_server": False,
-        #     "server_response": None,
-        #     "type": type
-        # }
-        # name start with number_string
-        userId = name.split('_')[0]
-        userName = name.split('_')[1]
-
-        self.db.insert(userId,userName, timestamp, percentage, image_path, 0, None, type, None)
-        print(f"Recorded {name} with {percentage:.2f}% similarity at {timestamp}")
-        # delete the image file
-        # os.remove(image_path)
+            self.db.insert(userId,userName, timestamp, percentage, image_path, 0, None, type, None)
+            print(f"Recorded {name} with {percentage:.2f}% similarity at {timestamp}")
+            # delete the image file
+            # os.remove(image_path)
+        except Exception as e:
+            print(f"Error: {e}")
         
 
     def start(self):
-        print("ImageAnalyzer started")
-        self.db = db.Database()
-            
-        if not os.path.exists(self.user_images_queue_path):
-            print(f"Error: The directory {self.user_images_queue_path} does not exist.")
-            return
-    
-        for image_file in os.listdir(self.user_images_queue_path):
-            image_path = os.path.join(self.user_images_queue_path, image_file)
-            if os.path.isfile(image_path) and image_file.endswith('.jpg'):
-                name, percentage = self.compare_faces(image_path)
-                if name != "Unknown":
-                    self.record_attendee(name, percentage, image_path)
-                elif percentage != -1:
-                    os.remove(image_path)
-        self.db.close()
-        print("ImageAnalyzer stopped")
+        try:
+            print("ImageAnalyzer started")
+            self.db = db.Database()
+                
+            if not os.path.exists(self.user_images_queue_path):
+                print(f"Error: The directory {self.user_images_queue_path} does not exist.")
+                return
+        
+            for image_file in os.listdir(self.user_images_queue_path):
+                image_path = os.path.join(self.user_images_queue_path, image_file)
+                if os.path.isfile(image_path) and image_file.endswith('.jpg'):
+                    name, percentage = self.compare_faces(image_path)
+                    if name != "Unknown":
+                        self.record_attendee(name, percentage, image_path)
+                    elif percentage != -1:
+                        try:
+                            os.remove(image_path)
+                        except Exception as e:
+                            print(f"Error: {e}")
+            self.db.close()
+            print("ImageAnalyzer stopped")
+        except Exception as e:
+            print(f"Error: {e}")
+            try:
+                self.db.close()
+            except Exception as e:
+                print(f"Error: {e}")
+            print("ImageAnalyzer stopped")
 # if __name__ == "__main__":
 #     analyzer = ImageAnalyzer('face_data/face_encodings.pkl', 'settings.json', 'user_images_queue')
 #     analyzer.start()
--- src/sync_data_to_server.py
+++ src/sync_data_to_server.py
@@ -29,99 +29,109 @@ class SyncDataToServer:
     
     def calculate_data(self):
         print("Calculating data")
-        self.db = db.Database()
-        # read settings.json to get sync_to_server value it is a boolean value
-        sync_to_server = False
         try:
-            with open(self.setting_path, 'r') as f:
-                settings = json.load(f)
-                sync_to_server = settings['sync_to_server']
-        except Exception as e:
-            print(f"Error: {e}")
-        
-        if not sync_to_server:
-            print("Sync to server is disabled")
-            return
-        
-        unsent_entries = self.db.get_all_unsent()
-        print(f"Unsent entries count: {len(unsent_entries)}")
-        
-        for entry in unsent_entries:
-            response = self.send_data_to_server(entry)
-            # if the response is string
-            if isinstance(response, str):
-                self.db.update_server_response(entry[0], response, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-                continue
+            self.db = db.Database()
+            # read settings.json to get sync_to_server value it is a boolean value
+            sync_to_server = False
             try:
-                if(response.get('status') == 'success'):
-                    self.db.delete(entry[0])
-                    continue
+                with open(self.setting_path, 'r') as f:
+                    settings = json.load(f)
+                    sync_to_server = settings['sync_to_server']
             except Exception as e:
                 print(f"Error: {e}")
-            # sleep for 0.1 second
-            time.sleep(0.2)
             
-        
-        self.db.close()
+            if not sync_to_server:
+                print("Sync to server is disabled")
+                return
+            
+            unsent_entries = self.db.get_all_unsent()
+            print(f"Unsent entries count: {len(unsent_entries)}")
+            
+            for entry in unsent_entries:
+                response = self.send_data_to_server(entry)
+                # if the response is string
+                if isinstance(response, str):
+                    self.db.update_server_response(entry[0], response, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                    continue
+                try:
+                    if(response.get('status') == 'success'):
+                        self.db.delete(entry[0])
+                        continue
+                except Exception as e:
+                    print(f"Error: {e}")
+                # sleep for 0.1 second
+                time.sleep(0.2)
+                
+            
+            self.db.close()
+        except Exception as e:
+            print(f"Error: {e}")
+            try:
+                self.db.close()
+            except Exception as e:
+                print(f"Error: {e}")
         print("Data calculation completed")
 
     def send_data_to_server(self, data):
-
-        dId = data[0]
-        dUserId = data[1]
-        dName = data[2]
-        dTime = data[3]
-        dType = data[4]
-        dPercentage = data[5]
-        dImage = data[6]
-        dSentToServer = data[7]
-        dServerResponse = data[8]
-        
-        
-        api_prefix = "http://kavin.test/api/v1/"
-        apiKey = ""
         try:
-            with open(self.setting_path, 'r') as f:
-                settings = json.load(f)
-                api_prefix = settings['api_prefix']
-                apiKey = settings['api_key']
+            dId = data[0]
+            dUserId = data[1]
+            dName = data[2]
+            dTime = data[3]
+            dType = data[4]
+            dPercentage = data[5]
+            dImage = data[6]
+            dSentToServer = data[7]
+            dServerResponse = data[8]
+            
+            
+            api_prefix = "http://kavin.test/api/v1/"
+            apiKey = ""
+            try:
+                with open(self.setting_path, 'r') as f:
+                    settings = json.load(f)
+                    api_prefix = settings['api_prefix']
+                    apiKey = settings['api_key']
+            except Exception as e:
+                print(f"Error: {e}")
+            
+            url = api_prefix + "entry"
+            
+            files = {}
+            imageName = dImage
+            if imageName:
+                isFileExist = os.path.isfile(imageName)
+                if isFileExist:
+                    openFile = open(imageName, 'rb')
+                    files = {'image': openFile}
+            
+            nData = {
+                'id': dId,
+                'user_id': dUserId,
+                'name': dName,
+                'timestamp': dTime,
+                'type': dType,
+                'percentage': dPercentage,
+                'sent_to_server': dSentToServer,
+                'server_response': dServerResponse
+            }
+            nData['api_key'] = apiKey
+            try:
+                response = requests.post(url, files=files, data=nData)
+                response.raise_for_status()
+                resp = response.json()
+                if resp['status'] == 'success':
+                    if os.path.exists(imageName):
+                        openFile.close()
+                        try:
+                            os.remove(imageName)
+                        except Exception as e:
+                            print(f"Error: {e}")
+                print(f"Response: {response.json()}")
+                return response.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Error123 sending data to server: {e}")
+                return str(e)
         except Exception as e:
             print(f"Error: {e}")
-        
-        url = api_prefix + "entry"
-        
-        files = {}
-        imageName = dImage
-        if imageName:
-            isFileExist = os.path.isfile(imageName)
-            if isFileExist:
-                openFile = open(imageName, 'rb')
-                files = {'image': openFile}
-        
-        nData = {
-            'id': dId,
-            'user_id': dUserId,
-            'name': dName,
-            'timestamp': dTime,
-            'type': dType,
-            'percentage': dPercentage,
-            'sent_to_server': dSentToServer,
-            'server_response': dServerResponse
-        }
-        nData['api_key'] = apiKey
-        try:
-            response = requests.post(url, files=files, data=nData)
-            response.raise_for_status()
-            resp = response.json()
-            if resp['status'] == 'success':
-                if os.path.exists(imageName):
-                    openFile.close()
-                    try:
-                        os.remove(imageName)
-                    except Exception as e:
-                        print(f"Error: {e}")
-            print(f"Response: {response.json()}")
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            print(f"Error123 sending data to server: {e}")
             return str(e)
